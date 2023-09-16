5ff06c01e1e48d56b994dd40a8f814ff714a8710
Chiraag Balu <86278218+chiraagbalu@users.noreply.github.com>
2023-09-16 15:31:11 -0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -408,8 +408,12 @@ def simulate_gbm_paths(s0, mu, sigma, n=24, T=30, num_paths=1000, plot=True):
         st.plotly_chart(fig_paths)
 
 
-#simulate_gbm_paths(s0=200, mu=0.0005, sigma=0.005, n=24, T=30, num_paths=10, plot=True)
+simulate_gbm_paths(s0=200, mu=0.0005, sigma=0.005, n=24, T=30, num_paths=10, plot=True)
 
+st.write("""Now that we've simulated some paths, let's look at the distribution of outcomes these paths might create! 
+
+Let's generate a lot more paths: 1000 should be a good number to start with
+""")
 
 
 
@@ -436,6 +440,7 @@ def simulate_gbm_paths_plotly_histogram_with_bins(s0, mu, sigma, n=24, T=30, num
     # Add GBM paths to the first subplot
     for i in range(num_paths):
         fig.add_trace(go.Scatter(x=t, y=S[i,:], mode='lines', name=f'Path {i+1}'), row=1, col=1)
+
     
     # Add a bar chart with bins and counts to the second subplot
     fig.add_trace(go.Bar(y=bin_edges[:-1], x=hist_values, orientation='h', name='End Values'), row=1, col=2)
@@ -462,7 +467,60 @@ simulate_gbm_paths_plotly_histogram_with_bins(s0=200, mu=0.0005, sigma=0.005, n=
 
 st.write("""
 
+Awesome! We've got a nice distribution that shows the different outcomes of our stock series.
+
 3. Figure out how much the option is worth for each price
+
+Let's do it again, and clearly mark the 'profitable' and 'unprofitable' zones: or where the price goes above the strike price.
+
+For now, we'll set the strike price to be 210.""")
+
+def simulate_gbm_paths_plotly_histogram_with_bins_and_color(s0, mu, sigma, n=24, T=30, num_paths=1000, num_bins=20, strike_threshold=200):
+    dt = 1/n
+    t = np.linspace(0, T, n*T+1)
+    S = np.zeros((num_paths, n*T+1))
+    
+    for i in range(num_paths):
+        W = np.random.standard_normal(size=n*T+1)
+        W = np.cumsum(W)*np.sqrt(dt)
+        X = (mu-0.5*sigma**2)*t + sigma*W
+        S[i,:] = s0*np.exp(X)
+    
+    # Calculate end values
+    end_values = S[:, -1]
+    
+    # Calculate histogram data
+    hist_values, bin_edges = np.histogram(end_values, bins=num_bins)
+    
+    # Create subplots with one row and two columns
+    fig = make_subplots(rows=1, cols=2, subplot_titles=('GBM Paths', 'End Value Histogram'), column_widths=[0.7, 0.3])
+    
+    # Add GBM paths to the first subplot
+    for i in range(num_paths):
+        fig.add_trace(go.Scatter(x=t, y=S[i,:], mode='lines', name=f'Path {i+1}'), row=1, col=1)
+
+    colors_strike = ['red' if x < strike_threshold else 'green' for x in bin_edges[:-1]]
+    
+    # Add a bar chart with bins and counts to the second subplot
+    fig.add_trace(go.Bar(y=bin_edges[:-1], x=hist_values, orientation='h', marker_color=colors_strike, name='End Values'), row=1, col=2)
+    
+    # Update layout
+    fig.update_layout(
+        title='Simulated Geometric Brownian Motion Paths with Histogram and Bins',
+        xaxis_title='Counts',
+        yaxis_title='Price',
+        xaxis2=dict(domain=[0.75, 1.0]),
+        yaxis2=dict(anchor='x2'),
+        showlegend=False,  # Set to False to avoid legend duplication
+        width=1000,
+        height=500,
+    )
+    
+    st.plotly_chart(fig)
+
+
+simulate_gbm_paths_plotly_histogram_with_bins_and_color(s0=200, mu=0.0005, sigma=0.005, n=24, T=30, num_paths=1000, strike_threshold=210)
+
 
 
 4. Find the average value of the option based on the probability distribution of prices
