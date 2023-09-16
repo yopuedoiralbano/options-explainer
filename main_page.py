d77d544cb661c63cc409f3e33927d16964537a26
Chiraag Balu <86278218+chiraagbalu@users.noreply.github.com>
2023-09-16 15:01:42 -0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -404,7 +404,7 @@ def simulate_gbm_paths(s0, mu, sigma, n=24, T=30, num_paths=1000, plot=True):
         st.plotly_chart(fig_paths)
 
 
-simulate_gbm_paths(s0=200, mu=0.0005, sigma=0.005, n=24, T=30, num_paths=100, plot=True)
+simulate_gbm_paths(s0=200, mu=0.0005, sigma=0.005, n=24, T=30, num_paths=10, plot=True)
 
 
 
